5cea6689c51f09c361f0d317d0dd669c6cfba5e9
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-05-04 19:18:46 -0300

    Add more meal details on meal_details widget

--- lib/widgets/meal_details.dart
+++ lib/widgets/meal_details.dart
@@ -24,6 +24,50 @@ class MealDetails extends StatelessWidget {
               height: 250,
               width: double.infinity,
             ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!
+              .copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 14),
+            ...meal.ingredients.map((ingredient) => 
+              ListTile(
+                dense: true,
+                visualDensity: const VisualDensity(vertical: -4),
+                title: Text(ingredient,
+                style: Theme.of(context).textTheme.headlineSmall!
+                .copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ))),
+            const SizedBox(height: 14),
+            const Divider(),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!
+              .copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            ...meal.steps.asMap().entries.map((entry) => 
+              ListTile(
+                dense: true,
+                visualDensity: const VisualDensity(vertical: -4),
+                title: Text(
+                  '${entry.key + 1}. ${entry.value}',
+                  style: Theme.of(context).textTheme.headlineSmall!
+                  .copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                ),
+              )),
           ],
         )
       ]),
